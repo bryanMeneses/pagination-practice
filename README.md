e3cb796cd0d3963f5106b42eaceee7e234092b41
Bryan Meneses <menesesbe93@Gmail.com>
2020-02-05 21:32:36 -0800

    posts are paginated

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Hello fellow programmer.
 
-I have grown too comfortable using my favorite tool, React.
+I have grown too comfortable using my favorite tool - React - so I am practicing vanilla Javascript.
 
-Have fun looking at my journey. :D
+Have fun looking at this small journey. :D
--- index.js
+++ index.js
@@ -1,28 +1,63 @@
+const elements = {
+  curPageEl: document.getElementById('current-page'),
+  nextPageEl: document.getElementById('next-page'),
+  prevPageEl: document.getElementById('prev-page'),
+  cardsContainer: document.getElementById('cards-container'),
+};
+
 const fetchPosts = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = await res.json();
+  return data;
+}
 
+const paginate = async () => {
+  const { curPageEl, nextPageEl, prevPageEl, cardsContainer } = elements;
+
+  let data = await fetchPosts();
+
+  let curPage = parseInt(curPageEl.innerText) - 1;
   let chunkedPosts = chunkPosts(data, 5)
 
-  const cardsContainer = document.getElementById('cards-container');
+  // initial map for screen load
+  mapPostsToHtml(cardsContainer, curPage, chunkedPosts);
 
-  const mainContent = chunkedPosts[0].map((cur, index) => {
-    return `
-      <div class="contact-card">
-        <h3 class="card-title">${cur.title}</h3>
-        <p class="card-body">${cur.body}</p>
-      </div>
-      `;
-  }).join('');
+  nextPageEl.addEventListener('click', () => {
+    if (curPage === chunkedPosts.length - 1) {
+      console.log('out of range array');
+      return;
+    }
+    curPage += 1;
+    curPageEl.innerText = curPage + 1;
+    mapPostsToHtml(cardsContainer, curPage, chunkedPosts)
+  })
 
-  cardsContainer.innerHTML = mainContent;
+  prevPageEl.addEventListener('click', () => {
+    if (curPage === 0) {
+      console.log('out of range array');
+      return;
+    }
+    curPage -= 1;
+    curPageEl.innerText = curPage + 1;
+    mapPostsToHtml(cardsContainer, curPage, chunkedPosts)
+  })
 }
+
+paginate();
+
+
+
+
+
+
+// Helper functions
 
 // chunk our data of 100 objects into arrays of 5 objects 
 // [{data} * 100 ] => [[data of 5] * 20]
 // should be 20 pages
 // return array of arrays
 const chunkPosts = (data, itemsPerPage) => {
+
   let chunkedData = [];
   let index = 0;
 
@@ -34,8 +69,16 @@ const chunkPosts = (data, itemsPerPage) => {
   return chunkedData;
 }
 
-// const paginatePost = (data, chunker) => {
+const mapPostsToHtml = (container, curPage, chunkedPosts) => {
 
-// }
+  const mainContent = chunkedPosts[curPage].map(cur => {
+    return `
+      <div class="contact-card">
+        <h3 class="card-title">${cur.title}</h3>
+        <p class="card-body">${cur.body}</p>
+      </div>
+      `;
+  }).join('');
 
-fetchPosts();
+  container.innerHTML = mainContent;
+}
